5a1291ee8b1f78784b7090e7e430da01d4726c0d
phil jones <interstar@gmail.com>
2014-03-26 23:13:46 -0300

    tweaking mansionhouse

--- examples/mansionhouse.py
+++ examples/mansionhouse.py
@@ -4,17 +4,21 @@ from basic_monosynth import basic_synth, twin_osc
 def noise_fm(id) :
     return fm(sigmult(noise(num()),1000),id)
     
+    
 if __name__ == '__main__' :
     script.clear()
     script.cr()
-    s1 = basic_synth(twin_osc(slider(1)),1)
+    s1 = basic_synth(twin_osc(slider("pitch1")),1)
     script.cr()
-    s2 = basic_synth(twin_osc(slider(1)),2)
+    s2 = basic_synth(twin_osc(slider("pitch2")),2)
     script.cr()
-    s3 = basic_synth(twin_osc(midi_notes()),3)
+    s3 = basic_synth(twin_osc(slider("pitch3")),3)
     script.cr()
-    s4 = basic_synth(noise_fm(slider(1)),4)
-    dac(s1,s2,s3,s4)
+    s4 = basic_synth(noise_fm(slider("pitch4fm")),4)
+    script.cr()
+    s5 = basic_synth(noise_fm(slider("pitch5fm")),5)
+    
+    dac(s1,s2,s3,s4,s5)
     print script.out()
     
    
