6c57554dcf96a9d726be19f2a21de6274664bf3e
phil jones <interstar@gmail.com>
2014-04-07 21:02:25 -0300

    using Abstraction

--- examples/mansionhouse.py
+++ examples/mansionhouse.py
@@ -6,19 +6,21 @@ def noise_fm(id) :
     
     
 if __name__ == '__main__' :
-    script.clear()
-    script.cr()
-    s1 = basic_synth(twin_osc(slider("pitch1")),1)
-    script.cr()
-    s2 = basic_synth(twin_osc(slider("pitch2")),2)
-    script.cr()
-    s3 = basic_synth(twin_osc(slider("pitch3")),3)
-    script.cr()
-    s4 = basic_synth(noise_fm(slider("pitch4fm")),4)
-    script.cr()
-    s5 = basic_synth(noise_fm(slider("pitch5fm")),5)
-    
-    dac_(s1,s2,s3,s4,s5)
-    print script.out()
+    with patch("basic_synth.pd") as taken :
+        outlet_ ( basic_synth(twin_osc(slider("pitch_$0",0,1000)),1) )
+        guiCanvas()
+
+    with patch("basic_fm.pd") as taken :
+        outlet_ ( basic_synth(noise_fm(slider("pitch_$0",0,1000)),4) ) 
+        guiCanvas()
+
+    with patch("mansion2.pd") as taken :
+        dac_( abstraction("basic_synth",800,50), 
+              abstraction("basic_synth",800,50), 
+              abstraction("basic_synth",800,50), 
+              abstraction("basic_fm",800,50), 
+              abstraction("basic_fm",800,50),
+              abstraction("basic_fm",800,50) 
+        )
     
    
