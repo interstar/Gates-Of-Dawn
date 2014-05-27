1c1327d0c9526db5f7bf5989ac3137c71f62232f
phil jones <interstar@gmail.com>
2014-05-27 15:53:45 -0300

    counter using with

--- examples/counter.py
+++ examples/counter.py
@@ -1,7 +1,6 @@
 from god import *
 
 if __name__ == '__main__' :
-    script.clear()
-    num(cycler(metronome(bang("metro counter"),"500"),"16"))
-    print script.out()    
+    with patch("counter.pd") as f :
+        vNum(cycler(metronome(bang("metro counter"),"500"),"16"))
 
